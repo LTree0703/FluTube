3967a43bdcdfd2c41ce24f6cfc8051b90b87c9af
LTree0703 <108115115+LTree0703@users.noreply.github.com>
2023-06-01 15:36:25 +0800

    Initial commit

---
+++ lib/splash_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen.navigate(
+        name: 'assets/splash.flr', // path to .flr animation file
+        next: (_) => const LoginPage(), // what to do after animation is completed
+        until: () => Future.delayed(const Duration(seconds: 1)), // wait for seconds
+        startAnimation: 'rotate_scale_color', // name the animation
+    );
+  }
+}
